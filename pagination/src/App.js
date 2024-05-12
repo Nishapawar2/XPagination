10bb13258a0e5007defd72b87fe56bbee57a1d5e
unknown <nisha.pawar2231@gmail.com>
2024-05-12 12:05:16 +0530

    first commit

---
+++ pagination/src/App.js
@@ -0,0 +1,68 @@
+import axios from "axios";
+import "./App.css";
+import { useEffect, useState } from "react";
+
+function App() {
+  const [employees, setEmployees] = useState([]);
+  const recordsPerPage=10;
+  const [currentPage,setCurrentPage]=useState(1);
+  const lastIndex=currentPage*recordsPerPage;
+  const firstIndex=lastIndex-recordsPerPage;
+  const records=employees.slice(firstIndex,lastIndex);
+  const numberOfpages=Math.ceil(employees.length/recordsPerPage);
+  const numbers=[...Array(numberOfpages+1).keys()].slice(1);
+  useEffect(() => {
+    axios
+      .get(
+        "https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json"
+      )
+      .then((res) => setEmployees(res.data))
+      .catch((error) => console.log("error fetching data", error));
+  }, []);
+
+  function prevPage(){
+    if(currentPage!==1){
+      setCurrentPage(currentPage-1);
+    }
+  }
+
+  function nextPage(){
+    if(currentPage!==numberOfpages){
+      setCurrentPage(currentPage+1);
+    }
+  }
+
+
+  return (
+    <div className="App">
+      
+        <h1>Employee Data Table</h1>
+        <table className="empTable">
+          <thead className="empHeading">
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Role</th>
+          </thead>
+          {records.map((emp) => {
+            return (
+              <tr key={emp.id}>
+                <td>{emp.id}</td>
+                <td>{emp.name}</td>
+                <td>{emp.email}</td>
+                <td>{emp.role}</td>
+              </tr>
+            );
+          })}
+        </table>
+        <div className="buttonDiv">
+          <button onClick={prevPage}>Previous</button>
+          <p>{currentPage}</p>
+          <button onClick={nextPage}>Next</button>
+        </div>
+      
+    </div>
+  );
+}
+
+export default App;
